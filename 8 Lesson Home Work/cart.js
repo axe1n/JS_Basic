741a5ea3a409b1668b77ff527b99bbdf0370320d
Alexey Shchadnev <shchadnev.a@gmail.com>
2022-06-09 19:53:45 +0300

    HW8 done

--- 8 Lesson Home Work/cart.js
+++ 8 Lesson Home Work/cart.js
@@ -6,8 +6,17 @@ cartIconEl.addEventListener('click', event => {
     cartContentsEl.classList.toggle('hidden');
 });
 
+function counter(count) {
+    return ++count;
+}
+
+function totalPrice(price) {
+    return totalAll + +price;
+}
 
 let cartCounter = 1;
+let itemQty = 1;
+let totalAll = 0;
 
 const featuredItemsEl = document.querySelector('.featuredItems');
 
@@ -25,22 +34,38 @@ featuredItemsEl.addEventListener('click', event => {
     }
 
     cart = event.target.closest('.featuredItem').dataset;
-
-    const itemName = document.querySelector('.itemName');
-    const itemQty = document.querySelector('.itemQty');
-    const itemPrice = document.querySelector('.itemPrice');
-
-
-
-
-
-    itemName.insertAdjacentHTML('beforeend', `
-    <div class="id${cart.id}">${cart.name}</div>
-    `);
+    const cartContentsEl = document.querySelector('.cartContents');
+    const itemNameEl = document.querySelector('.itemName');
+    const itemQtyEl = document.querySelector('.itemQty');
+    const itemPriceEl = document.querySelector('.itemPrice');
+    const totalEl = document.querySelector('.total');
+    const totalAllEl = document.querySelector('.totalAll').querySelector('span');
 
 
-    itemQty.insertAdjacentHTML('beforeend', `<div>${cart.id}</div>`);
-    itemPrice.insertAdjacentHTML('beforeend', `<div>${cart.price}</div>`);
+    if (!cartContentsEl.querySelector(`.qtyId${cart.id}`)) {
 
-    console.log(cart);
+        itemQtyEl.insertAdjacentHTML('beforeend', `
+        <div class="qtyId${cart.id}">${itemQty}</div>
+        `);
+        itemNameEl.insertAdjacentHTML('beforeend', `<div>${cart.name}</div>`);
+        itemPriceEl.insertAdjacentHTML('beforeend', `<div>${cart.price}</div>`);
+        totalEl.insertAdjacentHTML('beforeend', `
+        <div class="totalId${cart.id}">${cart.price}</div>
+        `);
+        totalAllEl
+            .innerHTML = `Цена за все товары: ${totalAll = totalPrice(cart.price)}`;
+    } else {
+        itemQtyEl
+            .querySelector(`.qtyId${cart.id}`)
+            .innerHTML =
+            counter(itemQtyEl.querySelector(`.qtyId${cart.id}`).innerHTML);
+
+        totalEl
+            .querySelector(`.totalId${cart.id}`)
+            .innerHTML =
+            `${itemQtyEl
+                .querySelector(`.qtyId${cart.id}`).innerHTML * cart.price}`;
+        totalAllEl
+            .innerHTML = `Цена за все товары: ${totalAll = totalPrice(cart.price)}`;
+    }
 });
