ffd36c353cebb225e09dfe40e4a54f342020a613
Alexey Shchadnev <shchadnev.a@gmail.com>
2022-06-09 21:25:28 +0300

    add comments

--- 8 Lesson Home Work/cart.js
+++ 8 Lesson Home Work/cart.js
@@ -1,33 +1,40 @@
 'use strict';
 
+// Реализовано появление блока корзины по клику на иконку корзины
 const cartIconEl = document.querySelector('.cartIcon');
 const cartContentsEl = document.querySelector('.cartContents');
 cartIconEl.addEventListener('click', event => {
     cartContentsEl.classList.toggle('hidden');
 });
 
-function counter(count) {
-    return ++count;
-}
-
-function totalPrice(price) {
-    return totalAll + +price;
-}
-
+const featuredItemsEl = document.querySelector('.featuredItems');
 let cartCounter = 1;
 let itemQty = 1;
 let totalAll = 0;
 
-const featuredItemsEl = document.querySelector('.featuredItems');
+// Функция считает количество товаров одного типа добавленных в корзину
+function counter(count) {
+    return ++count;
+}
+
+// Функция считает общую стоимость всех товаров в корзине
+function totalPrice(price) {
+    return totalAll + +price;
+}
+
 
 featuredItemsEl.addEventListener('click', event => {
     let cart = {};
     const addToCart = event.target;
+
+    // Если кликают не по кнопке добавить товар - ничего не происходит.
     const cartCounterEl = document.querySelector('.cartCounter');
     if (!addToCart.classList.contains('addToCart')) {
         return;
     }
 
+    // Если добавили какой-то товар в корзину - плявляется значок с количеством
+    // товаров в корзине
     cartCounterEl.innerHTML = cartCounter++;
     if (!cartCounterEl.hasOwnProperty('visible')) {
         cartCounterEl.style.visibility = 'visible';
@@ -41,9 +48,12 @@ featuredItemsEl.addEventListener('click', event => {
     const totalEl = document.querySelector('.total');
     const totalAllEl = document.querySelector('.totalAll').querySelector('span');
 
-
+    // Если в корзине такого товара ещё нет, то добавляется  имя, кол-во и цена.
+    // Если товар есть, то изменяется только кол-во. 
+    // Итого рассчитывается путем перемножения кол-ва товаров на цену.
+    // Общая цена рассчитывается прибавлением цены добавляемого товара каждый
+    // раз, когда товар добавляют в корзину.
     if (!cartContentsEl.querySelector(`.qtyId${cart.id}`)) {
-
         itemQtyEl.insertAdjacentHTML('beforeend', `
         <div class="qtyId${cart.id}">${itemQty}</div>
         `);
@@ -59,7 +69,6 @@ featuredItemsEl.addEventListener('click', event => {
             .querySelector(`.qtyId${cart.id}`)
             .innerHTML =
             counter(itemQtyEl.querySelector(`.qtyId${cart.id}`).innerHTML);
-
         totalEl
             .querySelector(`.totalId${cart.id}`)
             .innerHTML =
